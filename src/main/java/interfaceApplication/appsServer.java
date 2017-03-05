e4d28d61c9b45fd38f3d57aa4929fa97dbdad64a
putao520 <328742901@qq.com>
2017-03-05 22:57:33 +0800

    11

--- src/main/java/interfaceApplication/appsServer.java
+++ src/main/java/interfaceApplication/appsServer.java
@@ -7,11 +7,29 @@
  */
 package interfaceApplication;
 
+import java.util.UUID;
+import org.json.simple.JSONObject;
+
 import common.DBHelper;
+import common.JSONHelper;
+import security.md5;
 public class appsServer extends DBHelper{
 	//private db serviceServer;
 	public appsServer(){
 		super("localDB","services","id");
 		//serviceServer = getDB();
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public String insert(String appJSON){
+		JSONObject JSONObject = JSONHelper.string2json(appJSON);
+		JSONObject.put("appid",getAppid());
+		if( JSONObject.containsKey("id")){
+			JSONObject.remove("id");
+		}
+		return super.insert(JSONObject);
+	}
+	private String getAppid(){
+		return md5.getMD5(UUID.randomUUID().toString());
 	}
 }
